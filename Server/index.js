87f32f1a3a03438cc3491e491bcddb4b2c011435
lalatendu <lalatendupany.2000@gmail.com>
2023-10-07 12:02:25 +0530

    Projects Upadteed

--- Server/index.js
+++ Server/index.js
@@ -14,8 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const Myfunction = require("./Filecontroler/GoogleAuth");
 
 
-
-
 // Create an HTTP server
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -26,10 +24,11 @@ const wss = new WebSocket.Server({ server });
 
 // WebSocket connections
 wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
+    ws.on("message", (message) => {
     console.log(`Received: ${message}`);
-  });
+   });
 });
+
 function sendProgressUpdate(ws, progress) {
   if (ws.readyState === WebSocket.OPEN) {
     ws.send(JSON.stringify({ progress }));
@@ -38,40 +37,40 @@ function sendProgressUpdate(ws, progress) {
 
 
 // Api for downloard
-app.post("/start-downlord", async (req, res) => {
+app.post("/start-download", async (req, res) => {
   try {
-    const sourceFileId = "1H24_awOiHJ-kyaxOyi4aRDN0bZnrZOZE"; // Here replace your fileId
+      const sourceFileId = "1H24_awOiHJ-kyaxOyi4aRDN0bZnrZOZE"; // Here replace your fileId
 
-    const tempDownloadPath = "./temp/downloaded-video.mp4"; 
+      const tempDownloadPath = "./temp/downloaded-video.mp4"; 
 
-    // Download the file
-    res.setHeader("Content-Type", "application/json");
-    await Myfunction.downloadFile(sourceFileId, tempDownloadPath);
-    res.json({ message: "Downlord complted", progress: 100 });
-  } catch (error) {
-    console.error("Error in file transfer process:", error);
-    res.status(500).json({ message: "Error in file transfer process" });
-  }
+      // Download the file
+      res.setHeader("Content-Type", "application/json");
+      await Myfunction.downloadFile(sourceFileId, tempDownloadPath);
+      res.json({ message: "Downlord completed", progress: 100 });
+    } 
+    catch (error) {
+      console.error("Error in file transfer process:", error);
+      res.status(500).json({ message: "Error in file transfer process" });
+    }
 })
 
 // api for uploard with chunck 
 app.post("/start-process", async (req, res) => {
   try {
-    const destinationFolderId = "117EFmqswxoMczhyi6QS3oS6De5kOm0YY"; // Here replace your foldereId
-    const tempDownloadPath = "./temp/downloaded-video.mp4";
+      const destinationFolderId = "117EFmqswxoMczhyi6QS3oS6De5kOm0YY"; // Here replace your foldereId
+      const tempDownloadPath = "./temp/downloaded-video.mp4";
 
-    // Download the file
-    res.setHeader("Content-Type", "application/json");
+      // Download the file
+      res.setHeader("Content-Type", "application/json");
 
     // Start the chunked upload with progress callback
     await Myfunction.uploadFileChunked(
-      tempDownloadPath,
-      destinationFolderId,
-      async (progress, fileSize) => {
+        tempDownloadPath,
+        destinationFolderId,
+        async (progress, fileSize) => {
         
         // Calculate the percentage
         const percentage = (progress / fileSize) * 100;
-  
         // Send progress updates to all connected WebSocket clients
 
         wss.clients.forEach((client) => {
@@ -82,10 +81,13 @@ app.post("/start-process", async (req, res) => {
 
     // Send a completion message when the upload is finished
     res.json({ message: "Upload complete", progress: 100 });
-  } catch (error) {
+   } 
+
+  catch (error) {
     console.error("Error in file transfer process:", error);
     res.status(500).json({ message: "Error in file transfer process" });
   }
+  
 });
 
 
--- client/src/component/Uploardprogess.js
+++ client/src/component/Uploardprogess.js
@@ -1,30 +1,73 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
-
+import axios from "axios";
 export default function Uploardprogess() {
-    const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [loading2, setLoading2] = useState(false);
+  const [message, setMessage] = useState("");
+  const [message2, setMessage2] = useState("");
 
-    useEffect(() => {
-      const socket = new WebSocket("ws://localhost:4000"); // Replace with your server URL
+  useEffect(() => {
 
-      socket.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        setProgress(data.progress.toFixed(2));
-      };
+    const socket = new WebSocket("ws://localhost:4000"); // Replace with your server URL
 
-      return () => {
-        // Clean up WebSocket connection if needed
-      };
-    }, []);
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setProgress(data.progress.toFixed(2));
+    };  
+  }, []);
+  const dowonLoadProgress = async () => {
+        setLoading(true);
+        const res = await axios.post("http://localhost:4000/start-download"); // Replace with your server URL
+        if (res.status === 200) {
+        setLoading(false);
+
+        setMessage(res.data.message);
+
+        setMessage2("");
+      }
+    setLoading(false);
+  };
+  const upLoadProgress = async () => {
+      setLoading2(true);
+      const res = await axios.post("http://localhost:4000/start-process"); // Replace with your server URL
+      if (res.status === 200) {
+        setLoading2(false);
+        setMessage("");
+        setMessage2(res.data.message);
+        setProgress(0);
+      }
+    setLoading2(false);
+  };
+  console.log(loading, loading2);
   return (
     <div>
-        <h2>Upload Progress</h2>
-      <progress max="100" value={progress}></progress>
-      <p>{`${progress}%`}</p>
-   
-        
+      <div>
+        <h2>Download File</h2>
+        {!message ? (
+            <button onClick={dowonLoadProgress} disabled={loading}>
+                {loading ? "Downloading..." : "Start Download"}
+            </button>
+          ) : null}
+        <br />
+        <div>{message}</div>
+      </div>
+      {message ? (
+        <div style={{ marginTop: "20px" }}>
+            <h2>Upload Progress</h2>
+            <progress max="100" value={progress}></progress>
+            <p>{`${progress}%`}</p>
 
-      
+            <button onClick={upLoadProgress} disabled={loading2}>
+                {loading2 ? "Uploading..." : "Start Upload"}
+            </button>
+        </div>
+         ) : null}
+
+      <br />
+      <br />
+        <div>{message2}</div>
     </div>
-  )
+  );
 }
